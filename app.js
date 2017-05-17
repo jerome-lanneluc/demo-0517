c45ba7bee2b886b9eaad75b2653989a1c9dfae0c
jerome-lanneluc <jerome_lanneluc@fr.ibm.com>
2017-05-17 10:12:28 +0200

    Print Hello World

--- app.js
+++ app.js
@@ -21,9 +21,14 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+// print a welcome message
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
-	// print a message when the server starts listening
+  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
